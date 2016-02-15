c785f3878b28a0547dccaa5ecca3d9dff55c5311
Hawkeye Roe <r.roe@live.co.uk>
2016-02-15 17:36:27 +0000

    Added menu and GetWord

--- WordPlay.py
+++ WordPlay.py
@@ -2,7 +2,9 @@
 ##Word analysing program by Robert Roe
 ##150409241
 
-
+import OS
+import sys
+import random
 
 def Menu():
 	print ("""\t\tWelcome to Word Play
@@ -14,8 +16,29 @@ def Menu():
 		5. Quit\n\n""")
 	try:
 		choice = int(input("Enter choice: "))
+		return choice
 	except ValueError:
 		print("Number not valid")
 
+
+def getWord():
+	Word = str(input("Enter your word: "))
+	return Word
+	
+
 def main():
+	InputWord = ""
 	choice = Menu()
+	while(choice != 5):
+		if(choice == 1):
+			InputWord = getWord()
+		elif(choice == 2):
+			CheckLetterOccurences(InputWord)
+		elif(choice == 3):
+			CheckIsPalindrome(InputWord)
+		elif(choice == 4):
+			GenerateAnagram(InputWord)
+		else:
+			print("That was not an option.")
+		choice = Menu()
+	sys.exit()
