c9baede4d0daac674dbb723675eb4b66d3ad795c
Hawkeye Roe <r.roe@live.co.uk>
2016-02-16 08:59:16 +0000

    Tuesday morning commit

--- WordPlay.py
+++ WordPlay.py
@@ -26,11 +26,24 @@ def getWord():
 	Word = Word.lower()
 	return Word
 	
+def SplitWord(Word):
+	WordList = list(Word)
+	return WordList
+
 def CheckLetterOccurences(Word):
 	pass
 
-def CheckIsPalindrome(Word):
-	pass
+def CheckIsPalindrome(Word, WordList):
+	IsPalindrome = True
+	N = len(WordList)
+	while(IsPalindrome):
+		for i in range(0,int((N/2)), 1):
+			if(Word[i] == Word([N-i]-1)):
+				IsPalindrome = True
+			else:
+				IsPalindrome = False
+	if(IsPalindrome):
+		print(Word + "is a palindrome")
 
 def GenerateAnagram(Word):
 	pass
@@ -42,10 +55,11 @@ def main():
 		if(choice == 1):
 			InputWord = getWord()
 			print("Your word was " + InputWord)
+			WordList = SplitWord(InputWord)
 		elif(choice == 2):
 			CheckLetterOccurences(InputWord)
 		elif(choice == 3):
-			CheckIsPalindrome(InputWord)
+			CheckIsPalindrome(InputWord, WordList)
 		elif(choice == 4):
 			GenerateAnagram(InputWord)
 		else:
